b4503f378b99409f5e95c6ce51dd074122ca8af4
Kevin Solorio <kevin@detone8.com>
2014-06-22 10:39:17 -0500

    add CourseViewController

---
+++ Nerdfeed/CourseViewController.swift
@@ -0,0 +1,48 @@
+//
+//  CourceViewControllerTableViewController.swift
+//  Nerdfeed
+//
+//  Created by Kevin Solorio on 6/22/14.
+//  Copyright (c) 2014 BNR. All rights reserved.
+//
+
+import UIKit
+
+class CourseViewController: UITableViewController {
+
+    init(style: UITableViewStyle) {
+        super.init(style: style)
+        // Custom initialization
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // #pragma mark - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView?) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 0
+    }
+
+    override func tableView(tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 0
+    }
+
+
+}
